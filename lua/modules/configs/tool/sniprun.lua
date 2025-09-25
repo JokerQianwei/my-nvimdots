7d0be69c0a015f259cfcabd0fd353a068ab4de3b
JokerQianwei <1174665346@qq.com>
2025-09-26 02:26:55 +0800

    python 使用当前的 conda 环境运行

--- lua/modules/configs/tool/sniprun.lua
+++ lua/modules/configs/tool/sniprun.lua
@@ -1,8 +1,34 @@
+local function resolve_python_interpreter()
+	local conda_prefix = vim.env.CONDA_PREFIX
+	if conda_prefix and #conda_prefix > 0 then
+		local python_in_conda = conda_prefix .. "/bin/python"
+		if vim.loop.fs_stat(python_in_conda) then
+			return python_in_conda
+		end
+	end
+
+	local python3_path = vim.fn.exepath("python3")
+	if python3_path ~= "" then
+		return python3_path
+	end
+
+	local python_path = vim.fn.exepath("python")
+	if python_path ~= "" then
+		return python_path
+	end
+
+	return "python3"
+end
+
 return function()
 	require("modules.utils").load_plugin("sniprun", {
 		borders = "single",
 		inline_messages = 0,
-		interpreter_options = {},
+		interpreter_options = {
+			Python3_original = {
+				interpreter = resolve_python_interpreter(),
+			},
+		},
 		display = {
 			"Classic", -- "display results in the command-line area
 			"VirtualTextOk", -- "display ok results as virtual text (multiline is shortened)
