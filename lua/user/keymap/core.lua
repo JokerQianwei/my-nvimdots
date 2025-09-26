b3a79079a364ecf2f2e90b7757ba2ef567e0f11d
JokerQianwei <1174665346@qq.com>
2025-09-26 13:04:03 +0800

    s 移到第一个非空白字符

--- lua/user/keymap/core.lua
+++ lua/user/keymap/core.lua
@@ -4,6 +4,6 @@ local map_cmd = bind.map_cmd
 return {
 	-- 普通模式：; 移到行尾
 	["n|;"] = map_cmd("$"):with_silent():with_noremap():with_desc("cursor: Move to end of line"),
-	-- 普通模式：s 移到行首
-	["n|s"] = map_cmd("0"):with_silent():with_noremap():with_desc("cursor: Move to beginning of line"),
+	-- 普通模式：s 移到第一个非空白字符
+	["n|s"] = map_cmd("^"):with_silent():with_noremap():with_desc("cursor: Move to first non-blank"),
 }
